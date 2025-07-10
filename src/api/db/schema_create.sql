24804f400989728d7c8175f5331d66c9c32f6be5
IMadeACupcakeCode <leo0607gamer@gmail.com>
2025-07-10 17:36:50 -0300

    bd progresso

--- src/api/db/schema_create.sql
+++ src/api/db/schema_create.sql
@@ -14,3 +14,42 @@ create table
     "ChavePix" VARCHAR(255) not null,
     constraint "Users_pkey" primary key ("id")
   )
+
+create table
+  "public"."Admins" (
+    "id" serial not null,
+    "DataCriacao" timestamp not null default NOW(),
+    "CPF" varchar(255) not null,
+    "Nome" varchar(255) not null,
+    "Senha12" varchar(255) not null,
+    constraint "admins_pkey" primary key ("id")
+  )
+
+create table
+  "public"."Transfers" (
+    "id" serial not null,
+    constraint "Emissor" foreign key "Users_fkey" references Users("Users_pkey"),
+    constraint "Destinatario" foreign key "Users_fkey" references Users("Users_pkey"),
+    "Data" timestamp not null default NOW(),
+    "Valor" numeric not null, 
+    constraint "Transfers_pkey" primary key ("id")
+  )
+
+create table 
+  "public"."Emprestimos"(
+    "id" serial not null,
+    "Inicio" timestamp not null default NOW(),
+    "Valor" numeric not null, 
+    "Parcelas" integer not null,
+    "Fim" timestamp not null default,
+    constraint "Transfers_pkey" primary key ("id")
+  )
+
+create table
+  "public"."automatizacao"(
+    constraint "Emissor" foreign key "Users_fkey" references Users("Users_pkey"),
+    constraint "Destinatario" foreign key "Users_fkey" references Users("Users_pkey"),
+    "Inicio" timestamp not null default NOW(),
+    "Intervalo" integer not null,
+    constraint "ChavePix" foreign key "Users_fkey" references Users("Users_pkey"),
+  )
