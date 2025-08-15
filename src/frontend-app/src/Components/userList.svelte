ce89e277558aefdb5b95271ef25a9498d355e605
JoaoGabrielPereiraLopes <gabriel.pitito123@gmail.com>
2025-08-15 17:01:55 -0300

    Env

--- src/frontend-app/src/Components/userList.svelte
+++ src/frontend-app/src/Components/userList.svelte
@@ -61,6 +61,23 @@ import { onMount } from 'svelte';
                 </i>
             </div>
             <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquise Seu CPF" required />
+              <script context="module" lang="ts">
+                import type {Items} from '';
+                import { writable } from '';
+
+                export const dataItems = writable<Items[]>([]);
+
+                  const filterInfo = (term:string) => {
+                    dataItems.update(item => {
+                          item.filter(x => {
+                                return x.name.toLowerCase().includes(term.toLowerCase())
+                                || x.description.toLowerCase().includes(term.toLowerCase());
+                          })
+                          return dataItems;
+                        })
+                    }*/
+                export const dispatcher = {filterInfo};
+              </script>
         </div>
         </form>
       </button>
