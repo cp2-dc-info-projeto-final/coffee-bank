3741e51f0794f057323636814fda11f9bd18a2ad
IMadeACupcakeCode <leo0607gamer@gmail.com>
2025-09-04 15:38:34 -0300

    organizacao da cor do userList

--- src/frontend-app/src/Components/userList.svelte
+++ src/frontend-app/src/Components/userList.svelte
@@ -76,39 +76,39 @@
 </script>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg ">
-  <div class="w-full text-sm md:my-5 text-left rtl:text-right text-gray-500 dark:text-gray-400" >
-    <div class="flex flex-col p-5 items-center text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800 md:flex-row md:justify-between md:text-start rounded-t-lg">
+  <div class="w-full text-sm md:my-5 text-left rtl:text-right text-gray-400 " >
+    <div class="flex flex-col p-5 items-center text-lg font-semibold text-left rtl:text-right text-white bg-gray-800 md:flex-row md:justify-between md:text-start rounded-t-lg">
       <span class="text-xl text-center md:text-start">Usuários</span>
       <button on:click={() => goto(`/${user.id}`)} class=" hover: px-4 cursor-pointer">
         
-        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white text-xml">Search</label>
+        <label for="default-search" class="mb-2 text-sm font-medium text-white sr-only  text-xml">Search</label>
         <div class="relative">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <i class="w-4 h-4 text-gray-500 dark:text-gray-400 fa-solid fa-magnifying-glass">
+                <i class="w-4 h-4 text-gray-400  fa-solid fa-magnifying-glass">
                 </i>
             </div>
-         <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquise Seu CPF" bind:value={CPFFilter} on:input={CPFTransformer}/>
+         <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Pesquise Seu CPF" bind:value={CPFFilter} on:input={CPFTransformer}/>
         </div>
       </button>
       </div>
     <div class="grid grid-cols-1 md:grid-cols-2 rounded-sm">
     {#each users as user, i}
       <div
-        class="w-full flex flex-col p-6 bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700 rounded-b-sm
+        class="w-full flex flex-col p-6 bg-gray-800 border border-gray-700 shadow-sm  rounded-b-sm
               {i === users.length - 1 && users.length % 2 !== 0 ? 'md:col-span-2' : ''}"
       >
         <div class="flex flex-row text-center justify-center md:justify-start md:text-start">
           <img src={ user.Image ? user.Image: defaultImage } alt="" class="hidden md:me-4 md:rounded-full md:block md:w-27 md:h-27 md:aspect-square md:object-cover">
           <div>
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
           {user.Nome}
         </h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400">CPF: {user.CPF}</p>
+        <p class="font-normal text-gray-400">CPF: {user.CPF}</p>
         
-        <p class="font-normal text-gray-700 dark:text-gray-400">Chave Pix: {user.ChavePix}</p>
+        <p class="font-normal text-gray-400">Chave Pix: {user.ChavePix}</p>
         <div class="flex items-end">
-          <i on:click={() => goto(`/form/${user.id}`)} class="text-blue-600 dark:text-blue-500 hover:underline text-xl fa-solid fa-user-pen m-4 w-full"></i>
-          <i on:click={() => handleDelete(user.id)} class="font-medium text-blue-600 dark:text-blue-500 hover:underline fa-solid fa-trash m-4"></i>
+          <i on:click={() => goto(`/form/${user.id}`)} class="text-blue-500 hover:text-blue-900 text-xl fa-solid fa-user-pen m-4 w-full"></i>
+          <i on:click={() => handleDelete(user.id)} class="font-medium text-blue-500 hover:text-blue-900 fa-solid fa-trash m-4"></i>
         </div>
           </div>
         </div>
