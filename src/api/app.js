f24c0fda8d2023cd799963802a6aa66fb0ae4728
IMadeACupcakeCode <leo0607gamer@gmail.com>
2025-09-17 20:32:44 -0300

    Admin report impecável. Yeaa!

--- src/api/app.js
+++ src/api/app.js
@@ -15,7 +15,7 @@ var adminRouter = require('./routes/admin');
 var app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174'],
   credentials: true,
 }));
 
